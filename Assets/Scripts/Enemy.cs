fa1f489ff11463af300b93006c3bdb593cc471f2
AnthonyT-45 <atherrien54@gmail.com>
2024-10-22 23:51:14 -0700

    Modified enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
     public Transform target;
     public float moveSpeed = 2f;
     public float health = 10f;
+
+    public SpriteRenderer enemySprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,19 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.Translate(((direction * moveSpeed) * Time.deltaTime));
 
+        Debug.Log(target.position);
+
+        if (transform.position.x > target.position.x)
+        {
+            enemySprite.flipX = false;
+        }
+        else
+        {
+            enemySprite.flipX = true;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
