98d6f846f45810acda810eaffc90a955dc4f6cfb
AnthonyT-45 <atherrien54@gmail.com>
2024-12-08 23:40:38 -0800

    Successfully added two animator controllers for player child game objects, need to fix synchronization bug regarding animation lengths.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Numerics;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
-    public Animator animator;
+    public Animator lowerBodyAnimator;
+    public Animator upperBodyAnimator;
     public float health = 10f;
 
     private Rigidbody2D rb;
@@ -16,6 +18,8 @@ public class Player : MonoBehaviour
     private Vector2 lastDirection;
 
     private float attackX;
+
+    private bool isAttacking = false;
 
 
 
@@ -29,7 +33,10 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Movement();
+        if (!isAttacking)
+        {
+            Movement();
+        }
         Attack();
     }
 
@@ -65,20 +72,29 @@ public class Player : MonoBehaviour
         {
             lastDirection = movement;
         }
-        animator.SetFloat("Movement_X", movement.x);
-        animator.SetFloat("Movement_Y", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-        animator.SetFloat("Last_X", lastDirection.x);
-        animator.SetFloat("Last_Y", lastDirection.y);
+
+        lowerBodyAnimator.SetFloat("Movement_X", movement.x);
+        lowerBodyAnimator.SetFloat("Movement_Y", movement.y);
+        lowerBodyAnimator.SetFloat("Speed", movement.sqrMagnitude);
+        lowerBodyAnimator.SetFloat("Last_X", lastDirection.x);
+        lowerBodyAnimator.SetFloat("Last_Y", lastDirection.y);
+
+        upperBodyAnimator.SetFloat("Movement_X", movement.x);
+        upperBodyAnimator.SetFloat("Movement_Y", movement.y);
+        upperBodyAnimator.SetFloat("Speed", movement.sqrMagnitude);
+        upperBodyAnimator.SetFloat("Last_X", lastDirection.x);
+        upperBodyAnimator.SetFloat("Last_Y", lastDirection.y);
     }
 
     void Attack()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isAttacking)
         {
+            isAttacking = true;
 
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
 
             bool isLeft = mousePosition.x < transform.position.x;
 
@@ -91,14 +107,35 @@ public class Player : MonoBehaviour
                 attackX = 1f;
             }
 
-            lastDirection = new Vector2(attackX, 0);
-            animator.SetFloat("Attack_X", attackX);
-            animator.SetFloat("Last_X", lastDirection.x);
-            animator.SetFloat("Last_Y", lastDirection.y);
-            animator.SetFloat("Last_X", lastDirection.x);
-            animator.SetFloat("Last_Y", lastDirection.y);
-            animator.SetTrigger("Attack");
+            lastDirection = new Vector2(attackX, 0f);
 
+            lowerBodyAnimator.SetFloat("Attack_X", attackX);
+            lowerBodyAnimator.SetFloat("Last_X", lastDirection.x);
+            lowerBodyAnimator.SetFloat("Last_Y", lastDirection.y);
+            lowerBodyAnimator.SetTrigger("Attack");
+
+            upperBodyAnimator.SetFloat("Attack_X", attackX);
+            upperBodyAnimator.SetFloat("Last_X", lastDirection.x);
+            upperBodyAnimator.SetFloat("Last_Y", lastDirection.y);
+            upperBodyAnimator.SetTrigger("Attack");
+
+            StartCoroutine(EndAttack());
+
+
+        }
+
+        IEnumerator EndAttack()
+        {
+            AnimatorStateInfo stateInfo = upperBodyAnimator.GetCurrentAnimatorStateInfo(0);
+
+            while (!stateInfo.IsName("Attack"))
+            {
+                yield return null;
+                stateInfo = upperBodyAnimator.GetCurrentAnimatorStateInfo(0);
+            }
+            yield return new WaitForSeconds(stateInfo.length);
+
+            isAttacking = false;
 
 
 
